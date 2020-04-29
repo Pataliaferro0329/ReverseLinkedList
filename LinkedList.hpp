ced6d402dd916b64c53618a8cd3aa09fb968ff3e
Patrick Taliaferro <pataliaferro0329@gmail.com>
2020-04-29 08:39:16 -0400

    ready to start reverse traversal

---
+++ LinkedList.hpp
@@ -0,0 +1,102 @@
+//Author:    Patrick Taliaferro
+//Date:      April 29, 2020
+//File Name: LinkedList.hpp
+//Purpose:   Define the Linked List class which all the work will occur on
+
+#include <iostream>
+
+struct node {
+	int val;
+	node * next;
+};
+
+class LinkedList {
+
+	public:
+		LinkedList(node * head) { nHead = head; }
+		
+		~LinkedList()
+		{
+			while(nHead != NULL) pop_front();
+		}
+
+		int iFindLength()
+		{
+			if(nHead == NULL) return 0;
+			int iLength = 0;
+			node * nBuffer = nHead;
+			while(nBuffer != NULL)
+			{
+				iLength++;
+				nBuffer = nBuffer->next;
+			}
+			return iLength;
+		}
+
+		void push_back(int iVal)
+		{
+			node * newNode = new node();
+			newNode->val = iVal;
+			newNode->next = NULL;
+			if(nHead == NULL) 
+			{
+				nHead = newNode;
+				return;
+			}
+			node * nBuffer = nHead;
+			
+			while(nBuffer->next != NULL) nBuffer = nBuffer->next;
+
+			nBuffer->next = newNode;
+
+		}
+
+		void push_front(int iVal)
+		{
+			node * newNode = new node();
+			newNode->val = iVal;
+			newNode->next = nHead;
+			nHead = newNode;
+		}
+
+		void pop_back()
+		{
+			if(nHead == NULL) return;
+			else if(nHead->next == NULL)
+			{
+				delete nHead;
+				nHead = NULL;
+				return;
+			}
+			node * nBuffer = nHead;
+			while(nBuffer->next->next != NULL) nBuffer = nBuffer->next;
+			delete nBuffer->next;
+			nBuffer->next = NULL;
+		}
+
+		void pop_front()
+		{
+			if(nHead == NULL) return;
+			node * nTemp = nHead->next;
+			delete nHead;
+			nHead = nTemp;
+		}
+
+		void print_list_forward()
+		{
+			if(nHead == NULL)
+			{
+				std::cout << "done\n";
+				return;
+			}
+			node * nBuffer = nHead;
+			while(nBuffer != NULL)
+			{
+			       	std::cout << nBuffer->val << "\n";
+				nBuffer = nBuffer->next;
+			}
+		}
+
+	private:
+		node * nHead;
+};
--- main.cpp
+++ main.cpp
@@ -4,11 +4,35 @@
 //Purpose:    Traverse a Linked List in Reverse
 
 #include <iostream>
+#include "LinkedList.hpp"
+
 
 int main(int argv, char * argc[]) {
 
+	LinkedList llStart(NULL);
+	std::cout << "Length: " << llStart.iFindLength() << "\n";
+	llStart.push_front(5);
+	std::cout << "pushed 5 front\n";
+	std::cout << "Length: " << llStart.iFindLength() << "\n";
+	llStart.push_front(6);
+	std::cout << "pushed 6 front\n";
+	std::cout << "Length: " << llStart.iFindLength() << "\n";
+	llStart.print_list_forward();
 
-	std::cout << "Hello World!\n";
+	llStart.pop_front();
+	std::cout << "popped front\n";
+	llStart.print_list_forward();
+
+	llStart.push_front(4);
+	llStart.push_front(5);
+	llStart.push_front(6);
+	std::cout << "pushed 4,5,6 front \n";
+	llStart.print_list_forward();
+
+	llStart.pop_back();
+	llStart.pop_back();
+	std::cout << "popped back twice \n";
+	llStart.print_list_forward();
 
 	return 0;
 }
