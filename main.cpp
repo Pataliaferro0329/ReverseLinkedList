aa2d084dcdf2f43e22ee897b321bce4bdd8144f6
Patrick Taliaferro <pataliaferro0329@gmail.com>
2020-04-29 07:51:13 -0400

    Hello World Check

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Author:     Patrick Taliaferro
+//Date:       April 29, 2020
+//File Name:  main.cpp
+//Purpose:    Traverse a Linked List in Reverse
+
+#include <iostream>
+
+int main(int argv, char * argc[]) {
+
+
+	std::cout << "Hello World!\n";
+
+	return 0;
+}
